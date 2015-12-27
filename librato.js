3160f63197b8f9d597d3651cba8289fac344d8f5
Max Nowack <max@unsou.de>
2015-12-27 22:56:43 +0100

    more verbose logging

--- librato.js
+++ librato.js
@@ -1,5 +1,6 @@
 var config = require('./config');
 var request = require('request');
+var moment = require('moment');
 var cache = [];
 
 module.exports = function(id, value, time) {
@@ -10,5 +11,6 @@ module.exports = function(id, value, time) {
   };
   request.post('https://' + config.libratoEmail + ':' + config.libratoKey + '@metrics-api.librato.com/v1/metrics', {json: data}, function (error, response, body) {
     if(error) console.error(error);
-  })
+  });
+  console.log('[' + moment(time).format('DD.MM.YYYY HH:mm:ss') + '] ' + id + ': ' + value.toFixed(2));
 }
